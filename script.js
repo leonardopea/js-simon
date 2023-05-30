9993dab24ad9d8f8c398cc7462fadcf453fccc7c
leonardopea <leonardo.pea.1996@gmail.com>
2023-05-31 00:14:27 +0200

    stampo i 5 nuemri in console

---
+++ script.js
@@ -0,0 +1,16 @@
+let cinque = '';
+
+let numeri= document.getElementById('numeri');
+
+
+for(i=0; i<5; i++){
+
+    cinque =  parseInt(Math.random()*100) ;  
+
+    console.log(cinque);
+     
+     
+    
+}
+
+
