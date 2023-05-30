0fb0ce036abfdd3f61fc64550fd2bd105711946f
leonardopea <leonardo.pea.1996@gmail.com>
2023-05-31 00:41:21 +0200

    il bottone genera numeri in console

--- script.js
+++ script.js
@@ -3,14 +3,26 @@ let cinque = '';
 let numeri= document.getElementById('numeri');
 
 
-for(i=0; i<5; i++){
 
-    cinque =  parseInt(Math.random()*100) ;  
 
-    console.log(cinque);
-     
-     
+// recupero il bottone per generare i 5 numeri
+
+
+let random_button  = document.getElementById('bottone');
+
+
+// al click del bottone genero i 5 numeri 
+
+random_button.addEventListener('click', function(){
+    for(i=0; i<5; i++){
+
+
+        cinque =  parseInt(Math.random()*100) ;  
+
+        console.log(cinque)
     
-}
-
+        
+    }
+    
+})
 
